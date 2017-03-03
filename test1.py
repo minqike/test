1eea52a6793d09252425772a4c21e6fd85f92d19
minqike <minqike@foxmail.com>
2017-03-03 22:54:26 +0800

    ad3

--- test1.py
+++ test1.py
@@ -1,1 +1,35 @@
+# coding:utf8
 __author__ = 'minqk'
+
+
+class Animal(object):
+    def eat(self):
+        print("吃")
+
+
+class Cat(Animal):
+    def __init__(self, name):
+        self.name = name
+
+    def cry(self):
+        print("叫")
+
+
+class A(object):
+    def __init__(self):
+        print(1)
+
+
+class B(A):
+    def __init__(self):
+        print(2)
+
+
+class C(B):
+    def __init__(self):
+        print(super(C, self).__module__)
+
+
+d = C()
+
+
